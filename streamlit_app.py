27d8400e18374dc113127b88cce4b0fcc6a3acf6
ashfaq867 <97423424+ashfaq867@users.noreply.github.com>
2023-08-05 23:14:29 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,101 +1,28 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import subprocess  # <-- Import subprocess module here
+!pip install transformers -q
 
-# Every good app has a title, so let's add one:
-st.title('hello')
+!pip install gradio -q
 
-st.title('Hello World!')
-st.write('This is a simple text')
+from transformers import pipeline
 
-st.title("Hello")
-st.write("This is a simple test")
-# Expander section
-with st.expander("About"):
-    st.write("""Trying to add a data table, chart, sidebar button with
-             ballons, an image, text input & exploring tabs!""")
+p = pipeline("automatic-speech-recognition")
 
-# Sidebar section
-with st.sidebar:
-    st.subheader('This is a Sidebar')
-    st.write('Button with Balloons 🎈')
-    if st.button('Click me!✨'):
-        st.balloons()
-    else:
-        st.write(' ')
+import gradio as gr
+import time
 
-# Dataframe and Chart display section
-st.subheader('Interactive Data Table')
-df = pd.DataFrame(
-    np.random.randn(50, 3),  # generates random numeric values!
-    columns=["a", "b", "c"])
-st.dataframe(df)
+def transcribe(audio, state=""):
+    time.sleep(3)
+    text = p(audio)["text"]
+    state += text + " "
+    return state, state
 
-st.subheader('Bar Chart 📊')
-st.bar_chart(df)
-
-# Image upload and text input section
-st.subheader('An Image')
-st.image('https://www.scoopbyte.com/wp-content/uploads/2019/12/tom-and-jerry.jpg')
-
-st.subheader('Text Input')
-greet = st.text_input('Write your name, please!')
-st.write('👋 Hey!', greet)
-
-# Tabs section
-st.subheader('Tabs')
-tab1, tab2 = st.tabs(["TAB 1", "TAB 2"])
-
-with tab1:
-    st.write('WOW!')
-    st.image("https://i.gifer.com/DJR3.gif", width=400)
-
-with tab2:
-    st.write('Do you like ice cream? 🍨')
-    agree = st.checkbox('Yes! I love it')
-    disagree = st.checkbox("Nah! 😅")
-    if agree:
-        st.write('Even I love it 🤤')
-    if disagree:
-        st.write('You are boring 😒')
-
-
-def main():
-    st.title("Personalized Greeting App")
-    name = st.text_input("Enter your name:")
-
-    if name:
-        st.write(f"Hello, {name}! Welcome to your personalized Streamlit app.")
-
-    # Button to run file2.py
-    if st.button("Run Speech-to-Text App"):
-        st.write("Running the Speech-to-Text App...")
-        # Code from file 2
-        import gradio as gr
-        import time
-        from transformers import pipeline
-
-        p = pipeline("automatic-speech-recognition")
-
-        def transcribe(audio, state=""):
-            time.sleep(3)
-            text = p(audio)["text"]
-            state += text + " "
-            return state, state
-
-        gr.Interface(
-            fn=transcribe,
-            inputs=[
-                gr.inputs.Audio(source="microphone", type="filepath"),
-                'state'
-            ],
-            outputs=[
-                "textbox",
-                "state"
-            ],
-            live=True).launch()
-
-
-if __name__ == "__main__":
-    main()
+gr.Interface(
+    fn=transcribe,
+    inputs=[
+        gr.inputs.Audio(source="microphone", type="filepath"),
+        'state'
+    ],
+    outputs=[
+        "textbox",
+        "state"
+    ],
+    live=True).launch()
